ecb793e6fa4dfda561d91c5c0fb3b756a938608c
spencer-goff-quicken <spencer.goff@quicken.com>
2026-08-08 11:49:18 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-#hax here
-set -x  
 echo "hello, world!"
 export debug_1="${{ secrets.FLAG1 }}"
-export debug_2="${{ secrets.FLAG1 }}"
+export debug_2="${{ secrets.FLAG2 }}"
 export debug_3="${{ secrets.FLAG3 }}"
